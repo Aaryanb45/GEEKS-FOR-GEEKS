3db8962b1e388e8f521362fd90428b7a5c8ba5a8
ArYaN <113382989+Aaryanb45@users.noreply.github.com>
2025-04-25 21:42:53 +0530

    Create Sort 0s, 1s and 2s.cpp

---
+++ Sort 0s, 1s and 2s.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        int cnt0=0, cnt1=0, cnt2=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+                cnt0++;
+            else if(arr[i]==1)
+                cnt1++;
+            else
+                cnt2++;
+        }
+        for(int i=0;i<cnt0;i++)
+            arr[i]=0;
+        for(int i=cnt0;i<cnt0+cnt1;i++)
+            arr[i]=1;
+        for(int i=cnt0+cnt1;i<n;i++)
+            arr[i]=2;    
+    }
+};
